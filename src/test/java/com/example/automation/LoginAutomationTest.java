be1976806cf0ba52e6a09d30a90e1cb7adf83185
Pooja Karkhile <86223323+pooja1923@users.noreply.github.com>
2025-01-01 12:25:34 +0530

    Update LoginAutomationTest.java

--- src/test/java/com/example/automation/LoginAutomationTest.java
+++ src/test/java/com/example/automation/LoginAutomationTest.java
@@ -9,7 +9,7 @@ public class LoginAutomationTest {
     @Test 
     public void testLogin() { 
         // Set up the WebDriver 
-        System.setProperty("webdriver.chrome.driver", "C:\Users\Pooja\Downloads\chrome-win64\chrome-win64"); 
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Pooja\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"); 
         WebDriver driver = new ChromeDriver(); 
  
         try { 
