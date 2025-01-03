d2b95d3f80058cc297ab9af6254e391ecb1238bb
Pooja Karkhile <86223323+pooja1923@users.noreply.github.com>
2025-01-03 15:22:29 +0530

    Update LoginAutomationTest.java

--- src/test/java/com/example/automation/LoginAutomationTest.java
+++ src/test/java/com/example/automation/LoginAutomationTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver; 
 import org.openqa.selenium.WebElement; 
 import org.openqa.selenium.chrome.ChromeDriver; 
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.junit.jupiter.api.Test; 
 import static org.junit.jupiter.api.Assertions.assertEquals; 
 public class LoginAutomationTest { 
